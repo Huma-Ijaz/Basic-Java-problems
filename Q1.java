611faca778b6484f2144659111f77051e256d05e
Huma Ijaz <humaijaz193@gmail.com>
2025-04-22 05:47:13 +0500

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,11 @@
+
+
+public class Q1{
+public static void main(String[] args) {
+    int a , b , c ;              // declaration(chossing datatypes and variables)
+    a = 10 ; b = 20 ; c = 5;     //initialization(Givig values to variables) 
+    int sum = a + b + c ;
+    System.out.println("Sum of given numbers= " + sum);
+}
+    
+}
